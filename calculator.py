b63546e91d904ac97806e23528eab17806112d91
Shanvi008 <rhythmus209@gmail.com>
2026-04-12 18:22:09 +0530

    first calculator

---
+++ calculator.py
@@ -0,0 +1,16 @@
+# Ek Simple Calculator
+print("--- Welcome to My Python Calculator ---")
+
+# User se input lena
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+# Calculations
+jod = number1 + number2
+ghatana = number1 - number2
+guna = number1 * number2
+
+# Results dikhana
+print(f"Jod (Addition): {jod}")
+print(f"Ghatana (Subtraction): {ghatana}")
+print(f"Guna (Multiplication): {guna}")
